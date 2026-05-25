24e7e66529ba22b8ec4c25e0e89e6a12a3f8bcfd
iuri04 <2240104@my.ipleiria.pt>
2026-05-25 15:44:21 +0100

    registro

--- ProjetoDA/views/Registro.Designer.cs
+++ ProjetoDA/views/Registro.Designer.cs
@@ -39,22 +39,23 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(238, 39);
+            this.label1.Location = new System.Drawing.Point(211, 48);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.Size = new System.Drawing.Size(71, 16);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Criar conta";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(158, 158);
+            this.textBox1.Location = new System.Drawing.Point(146, 130);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(200, 22);
             this.textBox1.TabIndex = 1;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(158, 202);
+            this.textBox2.Location = new System.Drawing.Point(146, 201);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(200, 22);
             this.textBox2.TabIndex = 2;
@@ -62,28 +63,29 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(97, 161);
+            this.label2.Location = new System.Drawing.Point(185, 102);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.Size = new System.Drawing.Size(125, 16);
             this.label2.TabIndex = 3;
-            this.label2.Text = "label2";
+            this.label2.Text = "Nome de Utilizador:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(97, 205);
+            this.label3.Location = new System.Drawing.Point(197, 173);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.Size = new System.Drawing.Size(99, 16);
             this.label3.TabIndex = 4;
-            this.label3.Text = "label3";
+            this.label3.Text = "Palavra-passe:";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(158, 250);
+            this.button1.Location = new System.Drawing.Point(146, 254);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(200, 35);
             this.button1.TabIndex = 5;
-            this.button1.Text = "button1";
+            this.button1.Text = "Registrar";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Registro
@@ -99,6 +101,7 @@
             this.Controls.Add(this.label1);
             this.Name = "Registro";
             this.Text = "Registro";
+            this.Load += new System.EventHandler(this.Registro_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
