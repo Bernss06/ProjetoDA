389c76b73c72ea99ade506a8d56dc6ed75761b3e
Bernardo <bernas.henriques.lopoes@gmail.com>
2026-05-25 15:43:29 +0100

    controllers, modelos, fix

--- ProjetoDA/models/Compra.cs
+++ ProjetoDA/models/Compra.cs
@@ -12,17 +12,32 @@ namespace ProjetoDA
         public int Id { get; set; }
         public string NomeCompra { get; set; }
         public DateTime DataCriacao { get; set; }
-        public DateTime? DataFechada { get; set; } // Nullable, pois pode estar aberta
+        public DateTime? DataFechada { get; set; }
         public bool Fechada { get; set; }
         public decimal ValorTotal { get; set; }
         public DateTime? DataAlteracao { get; set; }
 
-        // Relações com o Utilizador indicadas no diagrama
+        // Relações com o Utilizador
         public Utilizador UserCria { get; set; }
         public Utilizador UserEdita { get; set; }
         public Utilizador UserFecha { get; set; }
+        public List<ItemCompra> Itens { get; set; }
 
-        public Compra() { }
+        // Construtor vazio obrigatório para o Entity Framework
+        public Compra()
+        {
+            Itens = new List<ItemCompra>(); // Garante que a lista não é nula
+        }
+
+        
+        public Compra(string nomeCompra, Utilizador userCria)
+        {
+            NomeCompra = nomeCompra;
+            UserCria = userCria;
+            DataCriacao = DateTime.Now;
+            Fechada = false;
+            Itens = new List<ItemCompra>();
+        }
 
         public override string ToString()
         {
