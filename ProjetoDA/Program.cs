d0975f4ed6d868fea504e959939713afd6d08ada
Leandro Ramos Silva <2241871@my.ipleiria.pt>
2026-06-02 14:26:09 +0100

    sdaad

--- ProjetoDA/Program.cs
+++ ProjetoDA/Program.cs
@@ -19,6 +19,9 @@ namespace ProjetoDA
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new views.FormRegistro());
+            Database.SetInitializer(
+                new DropCreateDatabaseIfModelChanges<ShoppingContext>()
+            );
         }
     }
 }
