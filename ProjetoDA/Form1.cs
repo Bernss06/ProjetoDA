84ec3f11e4a8347fc7a92dc0892cd9f1f399257a
Bernardo <bernas.henriques.lopoes@gmail.com>
2026-06-02 16:54:08 +0100

    asdfghjklç

--- ProjetoDA/Form1.cs
+++ ProjetoDA/Form1.cs
@@ -19,10 +19,23 @@ namespace ProjetoDA
 
         private void btnorcamento_Click(object sender, EventArgs e)
         {
-            // Abre a view de Orçamento com o ID do utilizador logado
+            // Abre a view de Orçamento e esconde o Form1
             var orcamentoForm = new views.Orcamento();
             orcamentoForm.Show();
             this.Hide();
+        }
+
+        // Adicione este método à classe Form1 para corrigir o erro CS1061.
+        // Este método permite que o formulário Orcamento atualize a label do orçamento no Form1.
+        public void AtualizarOrcamentoLabel(int valorMaximo)
+        {
+            // Exemplo: supondo que existe uma label chamada labelOrcamento no Form1.
+            // Substitua "labelOrcamento" pelo nome real da label que exibe o orçamento.
+            if (this.Controls.Find("labelOrcamento", true).FirstOrDefault() is Label label)
+            {
+                label.Text = valorMaximo.ToString("C");
+            }
+            // Caso queira atualizar outra label específica, substitua acima conforme necessário.
         }
     }
 }
