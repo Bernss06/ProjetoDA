91ce380fba89cada4e8a9cff14700811f1b18e1c
iuri04 <2240104@my.ipleiria.pt>
2026-06-02 15:24:42 +0100

    login funcional

--- ProjetoDA/controllers/LoginController.cs
+++ ProjetoDA/controllers/LoginController.cs
@@ -6,35 +6,25 @@ namespace ProjetoDA.controllers
 {
     internal class LoginController
     {
-        /// <summary>
-        /// Autentica um utilizador verificando username e password.
-        /// </summary>
-        /// <param name="username">Nome de utilizador</param>
-        /// <param name="password">Palavra-passe do utilizador</param>
-        /// <returns>ID do utilizador se autenticado com sucesso, caso contrário retorna -1</returns>
         public int AutenticarUtilizador(string username, string password)
         {
-            // Validações básicas
             if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
-            {
                 return -1;
-            }
 
             try
             {
                 using (var db = new ShoppingContext())
                 {
-                    // Procura o utilizador pelo username e password
-                    Utilizador utilizador = db.Utilizadores.FirstOrDefault(u => 
-                        u.Username == username && u.Password == password);
+                    Utilizador utilizador = db.Utilizadores.FirstOrDefault(u =>
+                        u.Username == username &&
+                        u.Password == password);
 
-                    // Retorna o ID se encontrado, caso contrário retorna -1
                     return utilizador != null ? utilizador.Id : -1;
                 }
             }
             catch (Exception ex)
             {
-                System.Diagnostics.Debug.WriteLine($"Erro ao autenticar utilizador: {ex.Message}");
+                System.Diagnostics.Debug.WriteLine($"Erro login: {ex.Message}");
                 return -1;
             }
         }
