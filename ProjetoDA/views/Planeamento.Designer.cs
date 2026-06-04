2a40fa20fb5ed5cc3df81d65bac40bfeaae1c7c3
Bernardo <bernas.henriques.lopoes@gmail.com>
2026-06-04 13:23:56 +0100

    planeamento

--- ProjetoDA/views/Planeamento.Designer.cs
+++ ProjetoDA/views/Planeamento.Designer.cs
@@ -30,33 +30,62 @@
         {
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnSavePlan = new System.Windows.Forms.Button();
+            this.btnRemoveItem = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.btnVoltarInicio = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.numArtigo = new System.Windows.Forms.NumericUpDown();
+            this.comboTipoArtigo = new System.Windows.Forms.ComboBox();
+            this.comboArtigo = new System.Windows.Forms.ComboBox();
+            this.btnAddItem = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.groupBox2.SuspendLayout();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numArtigo)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.button3);
-            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.btnSavePlan);
+            this.groupBox2.Controls.Add(this.btnRemoveItem);
             this.groupBox2.Controls.Add(this.listBox1);
-            this.groupBox2.Location = new System.Drawing.Point(438, 69);
+            this.groupBox2.Location = new System.Drawing.Point(438, 52);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(319, 380);
+            this.groupBox2.Size = new System.Drawing.Size(319, 397);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
+            // 
+            // btnSavePlan
+            // 
+            this.btnSavePlan.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSavePlan.Location = new System.Drawing.Point(65, 340);
+            this.btnSavePlan.Name = "btnSavePlan";
+            this.btnSavePlan.Size = new System.Drawing.Size(200, 40);
+            this.btnSavePlan.TabIndex = 8;
+            this.btnSavePlan.Text = "Guardar Planeamento";
+            this.btnSavePlan.UseVisualStyleBackColor = true;
+            // 
+            // btnRemoveItem
+            // 
+            this.btnRemoveItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemoveItem.Location = new System.Drawing.Point(65, 283);
+            this.btnRemoveItem.Name = "btnRemoveItem";
+            this.btnRemoveItem.Size = new System.Drawing.Size(200, 41);
+            this.btnRemoveItem.TabIndex = 7;
+            this.btnRemoveItem.Text = "Remover item selecionado";
+            this.btnRemoveItem.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(21, 22);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(268, 244);
+            this.listBox1.TabIndex = 0;
             // 
             // btnVoltarInicio
             // 
@@ -70,110 +99,89 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.numericUpDown1);
-            this.groupBox1.Controls.Add(this.comboBox2);
-            this.groupBox1.Controls.Add(this.comboBox1);
-            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.numArtigo);
+            this.groupBox1.Controls.Add(this.comboTipoArtigo);
+            this.groupBox1.Controls.Add(this.comboArtigo);
+            this.groupBox1.Controls.Add(this.btnAddItem);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(100, 69);
+            this.groupBox1.Location = new System.Drawing.Point(100, 52);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(319, 380);
+            this.groupBox1.Size = new System.Drawing.Size(319, 397);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Adicionar Item";
             // 
-            // label1
+            // numArtigo
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(44, 58);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.numArtigo.Location = new System.Drawing.Point(129, 195);
+            this.numArtigo.Name = "numArtigo";
+            this.numArtigo.Size = new System.Drawing.Size(166, 22);
+            this.numArtigo.TabIndex = 6;
+            this.numArtigo.ValueChanged += new System.EventHandler(this.numArtigo_ValueChanged);
             // 
-            // label2
+            // comboTipoArtigo
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(44, 205);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 16);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.comboTipoArtigo.FormattingEnabled = true;
+            this.comboTipoArtigo.Location = new System.Drawing.Point(129, 60);
+            this.comboTipoArtigo.Name = "comboTipoArtigo";
+            this.comboTipoArtigo.Size = new System.Drawing.Size(166, 24);
+            this.comboTipoArtigo.TabIndex = 5;
+            this.comboTipoArtigo.SelectedIndexChanged += new System.EventHandler(this.comboTipoArtigo_SelectedIndexChanged);
+            // 
+            // comboArtigo
+            // 
+            this.comboArtigo.FormattingEnabled = true;
+            this.comboArtigo.Location = new System.Drawing.Point(129, 126);
+            this.comboArtigo.Name = "comboArtigo";
+            this.comboArtigo.Size = new System.Drawing.Size(166, 24);
+            this.comboArtigo.TabIndex = 4;
+            this.comboArtigo.SelectedIndexChanged += new System.EventHandler(this.comboArtigo_SelectedIndexChanged);
+            // 
+            // btnAddItem
+            // 
+            this.btnAddItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddItem.Location = new System.Drawing.Point(82, 279);
+            this.btnAddItem.Name = "btnAddItem";
+            this.btnAddItem.Size = new System.Drawing.Size(171, 42);
+            this.btnAddItem.TabIndex = 3;
+            this.btnAddItem.Text = "Adicionar";
+            this.btnAddItem.UseVisualStyleBackColor = true;
+            this.btnAddItem.Click += new System.EventHandler(this.btnAddItem_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(44, 129);
+            this.label3.Location = new System.Drawing.Point(37, 197);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.Size = new System.Drawing.Size(80, 16);
             this.label3.TabIndex = 2;
-            this.label3.Text = "label3";
+            this.label3.Text = "Quantidade:";
             // 
-            // button1
+            // label2
             // 
-            this.button1.Location = new System.Drawing.Point(72, 314);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(200, 55);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(77, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tipo:";
             // 
-            // comboBox1
+            // label1
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(128, 49);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 24);
-            this.comboBox1.TabIndex = 4;
-            // 
-            // comboBox2
-            // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(128, 197);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(121, 24);
-            this.comboBox2.TabIndex = 5;
-            // 
-            // numericUpDown1
-            // 
-            this.numericUpDown1.Location = new System.Drawing.Point(128, 122);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
-            this.numericUpDown1.TabIndex = 6;
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(21, 22);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(268, 244);
-            this.listBox1.TabIndex = 0;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(65, 281);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(200, 33);
-            this.button2.TabIndex = 7;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(65, 325);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(200, 33);
-            this.button3.TabIndex = 8;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 129);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome do Artigo:";
             // 
             // Planeamento
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(798, 464);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.btnVoltarInicio);
             this.Controls.Add(this.groupBox2);
@@ -182,7 +190,7 @@
             this.groupBox2.ResumeLayout(false);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numArtigo)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -192,14 +200,14 @@
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Button btnVoltarInicio;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnSavePlan;
+        private System.Windows.Forms.Button btnRemoveItem;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
-        private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numArtigo;
+        private System.Windows.Forms.ComboBox comboTipoArtigo;
+        private System.Windows.Forms.ComboBox comboArtigo;
+        private System.Windows.Forms.Button btnAddItem;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
--- ProjetoDA/views/Planeamento.cs
+++ ProjetoDA/views/Planeamento.cs
@@ -7,28 +7,141 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ProjetoDA.controllers;
 
 namespace ProjetoDA.views
 {
     public partial class Planeamento : Form
     {
+        private readonly ArtigoController _artigoController;
+        private readonly TipoArtigoController _tipoArtigoController;
+        private Form1 _parent;
+
         public Planeamento()
         {
             InitializeComponent();
+            _artigoController = new ArtigoController();
+            _tipoArtigoController = new TipoArtigoController();
+            
+            this.Load += Planeamento_Load;
+            this.FormClosing += Planeamento_FormClosing;
+        }
+
+        // Construtor que recebe o Form1 como referência
+        public Planeamento(Form1 parent) : this()
+        {
+            _parent = parent;
+        }
+
+        private void Planeamento_Load(object sender, EventArgs e)
+        {
+            CarregarTiposArtigo();
+        }
+
+        private void CarregarTiposArtigo()
+        {
+            try
+            {
+                var tipos = _tipoArtigoController.getTiposArtigo();
+                comboTipoArtigo.DataSource = tipos;
+                comboTipoArtigo.DisplayMember = "Nome";
+                comboTipoArtigo.ValueMember = "Id";
+                comboTipoArtigo.SelectedIndex = -1;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao carregar tipos de artigo: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void comboTipoArtigo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboTipoArtigo.SelectedIndex == -1)
+            {
+                comboArtigo.DataSource = null;
+                return;
+            }
+
+            try
+            {
+                int tipoId = (int)comboTipoArtigo.SelectedValue;
+                var artigos = _artigoController.getArtigos()
+                    .Where(a => a.TipoArtigo != null && a.TipoArtigo.Id == tipoId)
+                    .ToList();
+
+                comboArtigo.DataSource = artigos;
+                comboArtigo.DisplayMember = "Nome";
+                comboArtigo.ValueMember = "Id";
+                comboArtigo.SelectedIndex = -1;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao carregar artigos: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void btnVoltarInicio_Click(object sender, EventArgs e)
         {
-            // Se houver um Form1 que abriu este formulário, volta a mostrar esse Form1
-            
-           
-                // Caso contrário, cria uma nova instância (comportamento antigo)
+            // Se temos referência ao Form1 parent, mostra-o novamente
+            if (_parent != null)
+            {
+                _parent.Show();
+            }
+            else
+            {
+                // Caso contrário, cria uma nova instância
                 Form1 form1 = new Form1();
                 form1.Show();
-            
+            }
 
             // Esconde o formulário atual
             this.Hide();
+        }
+
+        private void comboArtigo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Pode ser usado para atualizar outras informações do artigo selecionado
+        }
+
+        private void numArtigo_ValueChanged(object sender, EventArgs e)
+        {
+            // Tratamento de mudança de quantidade
+        }
+
+        private void btnAddItem_Click(object sender, EventArgs e)
+        {
+            if (comboArtigo.SelectedIndex == -1)
+            {
+                MessageBox.Show("Por favor, selecione um artigo.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (numArtigo.Value <= 0)
+            {
+                MessageBox.Show("Por favor, introduza uma quantidade válida.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                // Adicionar o item à listBox1
+                string artigo = comboArtigo.Text;
+                int quantidade = (int)numArtigo.Value;
+                listBox1.Items.Add($"{artigo} - Qtd: {quantidade}");
+
+                // Limpar os campos
+                comboArtigo.SelectedIndex = -1;
+                numArtigo.Value = 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao adicionar item: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Planeamento_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Não é necessário fazer nada especial aqui                
         }
     }
 }
